4a15b8236333610b5917354ab8a57ab4111f3cf2
aks2002aks <102814881+aks2002aks@users.noreply.github.com>
2024-02-16 20:52:04 +0530

    new chnage

--- controllers/ImageController.ts
+++ controllers/ImageController.ts
@@ -19,12 +19,15 @@ async function saveImageAndGetImageUrl(
   try {
     const { url } = req.body;
 
-    const query = `https://api.pikwy.com?token=${process.env.PIKWY_API_KEY}&url=${url}&response_type=image&full_page=1`;
+    // Optimized Axios request with stream response type
+    const resp = await axios.get<stream.Readable>(
+      `https://api.pikwy.com?token=${process.env.PIKWY_API_KEY}&url=${url}&response_type=image&full_page=1`,
+      {
+        responseType: "stream",
+      }
+    );
 
-    const resp = await axios.get<stream.Readable>(query, {
-      responseType: "stream",
-    });
-
+    // Cloudinary upload configuration
     const cloudinaryResponse: UploadApiResponse = await new Promise(
       (resolve, reject) => {
         const uploadStream = cloudinary.uploader.upload_stream(
@@ -33,33 +36,25 @@ async function saveImageAndGetImageUrl(
             resource_type: "image",
           },
           (error, result) => {
-            if (result) resolve(result);
-            else {
-              reject(new Error("Upload result is undefined"));
-              res
-                .status(500)
-                .json({ success: false, error: "Internal server error" });
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result as UploadApiResponse);
             }
           }
         );
 
-        if (resp.data) {
-          resp.data.pipe(uploadStream);
-        } else {
-          reject(new Error("Empty response data from Pikwy API"));
-          res
-            .status(500)
-            .json({ success: false, error: "Internal server error" });
-        }
+        resp.data.pipe(uploadStream);
       }
     );
 
+    // Return the secure URL of the uploaded image
     const imageUrl: string = cloudinaryResponse.secure_url;
     res.status(200).json({ success: true, imageUrl });
   } catch (error) {
-    res
-      .status(500)
-      .json({ success: false, error: "Internal server error" + error });
+    // Improved error handling with informative error messages
+    console.error("Error occurred:", error);
+    res.status(500).json({ success: false, error: "Internal server error" });
   }
 }
 
